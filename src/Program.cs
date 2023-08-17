9dcfb609e76a86d89a49049d0cd574956bc4e109
ShugoYoko <yokotnaname@gmail.com>
2023-08-17 18:33:39 +0900

    プロジェクト ファイルを追加します。

---
+++ src/Program.cs
@@ -0,0 +1,30 @@
+﻿
+namespace src
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //ユーザー入力
+            Console.Write("Research folder path:");
+            var targetFolder = Console.ReadLine();
+            Console.Write("Start date(yyyy/MM/dd):");
+            var startDate = Console.ReadLine();
+            Console.Write("End date(yyyy/MM/dd):");
+            var endDate = Console.ReadLine();
+
+            var search = new SearchPaths(targetFolder, startDate, endDate);
+            foreach (var path in search.GetAllFiles())
+            {
+                Console.WriteLine(path);
+            }
+
+            //終了待機
+            Console.WriteLine("-------------------");
+            Console.ReadLine();
+
+        }
+    }
+}
+
